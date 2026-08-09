eaade3793d99377ad85a934d1e260bfe142c3086
ankit28k <ankit25210kumar@gmail.com>
2026-08-09 13:02:23 +0530

    Time: 0 ms (100%), Space: 15.1 MB (22.05%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -14,27 +14,50 @@ public:
     vector<int> rightSideView(TreeNode* root) {
 
         if(root==NULL) return {};
-        map<int,TreeNode*> mp;
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,0});
+
+        //TC-> O(nlogn) , SC->(n)
+        // map<int,TreeNode*> mp;
+        // queue<pair<TreeNode*,int>> q;
+        // q.push({root,0});
+
+        // while(!q.empty()){
+        //     auto t =q.front();
+        //     q.pop();
+
+        //     auto node = t.first;
+        //     int x = t.second;
+        //     if(mp.find(x) == mp.end()){
+        //         mp[x] = node;
+        //     }
+        //     if(node->right) q.push({node->right,x+1});
+        //     if(node->left) q.push({node->left,x+1});
+        // }
+
+        // vector<int> ans;
+        // for(auto it:mp){
+        //     ans.push_back(it.second->val);
+        // }
+        // return ans;
+        
+        //TC-> O(N) , SC->(n)
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<int> ans;
 
         while(!q.empty()){
-            auto t =q.front();
-            q.pop();
+            int sz = q.size();
+            for(int i=0;i<sz;i++){
+                auto t =q.front();
+                q.pop();
 
-            auto node = t.first;
-            int x = t.second;
-            if(mp.find(x) == mp.end()){
-                mp[x] = node;
+                if(i==0) ans.push_back(t->val);
+
+                if(t->right) q.push(t->right);
+                if(t->left) q.push(t->left);
             }
-            if(node->right) q.push({node->right,x+1});
-            if(node->left) q.push({node->left,x+1});
+            
         }
 
-        vector<int> ans;
-        for(auto it:mp){
-            ans.push_back(it.second->val);
-        }
         return ans;
     }
 };
